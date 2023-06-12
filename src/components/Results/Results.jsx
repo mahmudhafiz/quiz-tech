5470d0d90f88138088fc0fd971f8ddd9c7d72def
syedhafiz <macbook@MACBOOKs-MacBook-Pro.local>
2023-06-12 15:58:07 +0600

    Initial complete

--- src/components/Results/Results.jsx
+++ src/components/Results/Results.jsx
@@ -1,31 +1,54 @@
 import React from 'react';
+import { useNavigate, useOutletContext } from 'react-router-dom';
 
 const Results = () => {
-  let score = 4;
+  const resultData = useOutletContext();
+  const navigate = useNavigate();
+  const { greenAns, totalQ, nameQuiz } = resultData;
+  const score = (greenAns / totalQ) * 100;
+  let text;
+
+  if (score >= 80) {
+    text = 'a master';
+  }
+  else if (score >= 60) {
+    text = 'almost a master';
+  }
+  else if (score >= 40) {
+    text = 'soon to be a master';
+  }
+  else {
+    text = 'a noob';
+  }
+
+  const backHome = () => {
+    navigate('/home');
+  };
+
   return (
     <div className='flex justify-center text-center'>
-      <div className='w-[1250px] h-[75vh] bg-[#1e1f32] mt-2 p-12'>
-        <div className='h-[100%] bg-[#0D0F3B] p-2 text-cyan-50'>
+      <div className='w-[1250px] h-[75vh] bg-[#9703a1] mt-2 p-12'>
+        <div className='h-[100%] bg-[#0d005f] p-2 text-cyan-50'>
           <div className='mx-auto py-4'>
             <h1 className='mt-4'>
               <b className='text-2xl text-red-800'>Congratulations</b>, 
-              you have completed the quiz and {score > 5 ? 'a master': 'almost a master'} of JS.
+              you have completed the quiz and {text} of {nameQuiz}.
             </h1>
           </div>
           <div className='mx-auto py-20'>
             <h2>
-              You have correctly answered 5 questions out of 11! <br />& achieved
+              You have correctly answered <b className='text-2xl'>{greenAns}</b> questions out of <b className='text-2xl'>{totalQ}</b>. <br />& achieved
             </h2>
             <p className='mt-8 text-xl'>
-              <b className='text-3xl'>94</b> percentile.
+              <b className='text-3xl'>{Math.ceil(score)}</b> percentile.
             </p>
           </div>
           <div>
             <h2>
               Wanna give it another try?
             </h2>
-            <button className='bg-[#5e057c] text-white py-2 mx-auto w-[20%] rounded-md mt-9'>
-              Go back to home!
+            <button onClick={backHome} className='bg-[#6c1a06] text-white py-2 mx-auto w-[15%] rounded-md mt-9'>
+              Go back!
             </button>
           </div>
         </div>
