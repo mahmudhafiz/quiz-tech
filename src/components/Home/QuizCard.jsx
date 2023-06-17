b49f62e5908dd2f20a75d095554e713ce45d7e94
syedhafiz <macbook@mhafiz.local>
2023-06-17 20:26:29 +0600

    Color tweaks 2

--- src/components/Home/QuizCard.jsx
+++ src/components/Home/QuizCard.jsx
@@ -10,7 +10,7 @@ const QuizCard = ({dataForQuiz}) => {
     }
 
     return (
-        <div className='bg-[#fdf0d5] text-black w-[16rem] h-[20rem] rounded-md shadow-inner hover:shadow-xl hover:scale-105 duration-300'>
+        <div className='bg-gradient-to-bl from-red-700 to-cyan-600 text-white w-[16rem] h-[20rem] rounded-md shadow-inner hover:shadow-xl hover:scale-105 duration-300'>
             <div className='flex flex-col justify-around items-center p-8'>
                 <div className='w-12 h-12 border rounded mb-5 bg-transparent'>
                     <img src={logo} alt="" />
@@ -18,10 +18,10 @@ const QuizCard = ({dataForQuiz}) => {
                 <h1 className='font-bold text-2xl my-4'>
                     {name}
                 </h1>
-                <p className='text-md mt-2 text-[#a8dadc] mb-2'>
+                <p className='text-lg mt-2 text-[#ffffff] mb-2'>
                     {total} questions to tackle!
                 </p>
-                <button onClick={handleEvent} className='bg-[#38b000] text-white py-2 mx-auto w-[70%] rounded-md mt-9'>
+                <button onClick={handleEvent} className='bg-[#dbdada] text-black font-bold py-2 mx-auto w-[70%] rounded-md mt-9'>
                     Start
                 </button>
             </div>
--- tailwind.config.js
+++ tailwind.config.js
@@ -4,7 +4,11 @@ module.exports = {
     "./src/**/*.{js,jsx,ts,tsx}"
   ],
   theme: {
-    extend: {},
+    extend: {
+      backgroundImage: {
+        'gradient-radial': 'linear-gradient(to right, #2c3e50, #4ca1af)',
+      },
+    },
   },
   plugins: [],
 }
